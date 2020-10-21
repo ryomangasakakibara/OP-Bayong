08680c7d1cdfbb9a539f10fef2b64c57690cee01
ryomangasakakibara <60686151+ryomangasakakibara@users.noreply.github.com>
2020-10-21 11:26:35 +0800

    top-header

---
+++ OP-Bayong/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { FooterComponent } from './landingpage/footer/footer.component';
+import { HeaderComponent } from './landingpage/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingpageComponent,
+    FooterComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
